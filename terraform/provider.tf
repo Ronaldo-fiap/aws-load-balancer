d7fcb69b72068d80c2e6ac2251d9c48634dc2451
Ronaldo-fiap <rm564630@fiap.com.br>
2025-08-20 21:46:07 -0300

    Primeiro Commit Ronaldo

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 # PROVIDER
 terraform {
 
-  required_version = "~> 1.12.2"
+  required_version = "1.13.0"
 
   required_providers {
     aws = {
@@ -9,12 +9,10 @@ terraform {
       version = "~> 6.9.0"
     }
   }
-
-}
-
-provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "iac"
+  backend "s3" {
+    bucket       = "aws-s3-tfstate-<seuusuario>"  # SUBSTITUA
+    key          = "tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
 }
